9e13211b13417557d3f834bfeb0432ce595a1752
Ram11Coder <rambeliver11@gmail.com>
2025-04-09 23:52:35 +0530

    solved using built-in method

--- Strings/swap_case.py
+++ Strings/swap_case.py
@@ -1,13 +1,16 @@
+# def swap_case(s):
+#     res = ""
+#     for i in s:
+#         if i.isupper():
+#             res += i.lower()
+#         elif i.islower():
+#             res += i.upper()
+#         else:
+#             res += i
+#     return res
+
 def swap_case(s):
-    res = ""
-    for i in s:
-        if i.isupper():
-            res += i.lower()
-        elif i.islower():
-            res += i.upper()
-        else:
-            res += i
-    return res
+    return s.swapcase()
 
 
 if __name__ == '__main__':
